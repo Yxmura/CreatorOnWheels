09b6e7af94ddef7bf7bcd6be554325d0ba093d0b
Yamura3 <marceldoethetweer.be@gmail.com>
2025-01-25 16:04:48 +0100

    1.1.4
    fixed footer assets, optimized js, footer cart bugfix

--- build/scripts.js
+++ build/scripts.js
@@ -44,8 +44,3 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
-
-document.getElementById("close-banner").addEventListener("click", function (event) {
-    event.stopPropagation();  // Prevent clicking on the button itself from triggering the close event
-    this.closest("button").style.display = "none";  // Hide the button (banner)
-  });
